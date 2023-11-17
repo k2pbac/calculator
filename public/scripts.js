29858a1944fd112f7a8b2727bb602f577182ae17
k2pbac <krisbachan123@hotmail.com>
2023-11-17 14:59:29 -0500

    fixed calcs

--- public/scripts.js
+++ public/scripts.js
@@ -1,9 +1,39 @@
 $(document).ready(function () {
-  $(".calculator-form form > input").on("click", (res) => {
-    const val = handleInput(res.target.value);
+  let sign = "";
+  let runningTotal = 0;
+  let runningVal = [];
+  let index = 0;
+
+  $(".calculator-form form > input").on("click", (element) => {
+    const val = handleInput(element.target.value);
+    if (!!runningVal[index]) runningVal[index] += "" + val;
+    else runningVal.push(val);
+    const currentVal = runningVal[index];
+    console.log(index);
+    if (sign == "") $(".total").val(currentVal);
+    else {
+      if (sign === "+") runningTotal = add(runningTotal, runningVal[index]);
+      else if (sign === "-")
+        runningTotal = subtract(runningTotal, runningVal[index]);
+      else if (sign === "/")
+        runningTotal = divide(runningTotal, runningVal[index]);
+      else if (sign === "*")
+        runningTotal = multiply(runningTotal, runningVal[index]);
+      $(".total").val($(".total").val() + " " + currentVal);
+    }
+    console.log({ runningTotal, runningVal });
+  });
+
+  $(".calculator-form form > button").on("click", (element) => {
+    sign = element.target.value;
     const currentVal = $(".total").val();
-    if (currentVal == "0") $(".total").val(val);
-    else $(".total").val(currentVal + val);
+    if (sign !== "=") {
+      if (index == 0) runningTotal += runningVal[index];
+      index++;
+      $(".total").val(currentVal + " " + sign);
+    } else {
+      $(".total").val(runningTotal);
+    }
   });
 });
 
@@ -14,7 +44,20 @@ const handleInput = (input) => {
     const num = parseFloat(input);
     if (typeof num == "number") {
       total = num;
-    } else return 0;
+    }
   }
   return total;
+};
+
+const add = (a, b) => {
+  return parseFloat(a) + parseFloat(b);
+};
+const subtract = (a, b) => {
+  return parseFloat(a) - parseFloat(b);
+};
+const divide = (a, b) => {
+  return parseFloat(a) / parseFloat(b);
+};
+const multiply = (a, b) => {
+  return parseFloat(a) * parseFloat(b);
 };
