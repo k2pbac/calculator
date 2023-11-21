1eb8a3cdfe3248eb8c8c8ac9c79f038fcd3d039c
k2pbac <krisbachan123@hotmail.com>
2023-11-21 09:10:16 -0500

    styles and tests

--- public/scripts.js
+++ public/scripts.js
@@ -3,36 +3,98 @@ $(document).ready(function () {
   let runningTotal = 0;
   let runningVal = [];
   let index = 0;
+  let isOnDiv = false;
+  var divPos = {};
+  var offset = $("#draggable").offset();
 
+  $.fn.rotationInfo = function () {
+    var el = $(this),
+      tr =
+        el.css("-webkit-transform") ||
+        el.css("-moz-transform") ||
+        el.css("-ms-transform") ||
+        el.css("-o-transform") ||
+        "",
+      info = { rad: 0, deg: 0 };
+    if ((tr = tr.match("matrix\\((.*)\\)"))) {
+      tr = tr[1].split(",");
+      if (typeof tr[0] != "undefined" && typeof tr[1] != "undefined") {
+        info.rad = Math.atan2(tr[1], tr[0]);
+        info.deg = parseFloat(((info.rad * 180) / Math.PI).toFixed(1));
+      }
+    }
+    return info;
+  };
+
+  $("#draggable").mouseenter(function () {
+    isOnDiv = true;
+  });
+  $("#draggable").mouseleave(function () {
+    isOnDiv = false;
+  });
+  $("#draggable").draggable(
+    { containment: ".calculator-form" },
+    {
+      start: function () {
+        console.log("start");
+        const obj = $(this);
+      },
+      drag: function (event, ui) {
+        const obj = $(this);
+        console.log(obj.rotationInfo());
+
+        divPos = {
+          left: event.pageX - offset.left,
+          top: event.pageY - offset.top,
+        };
+        let x = Math.abs(16 - divPos["left"]);
+        let y = 304 - divPos["top"];
+        console.log({ x, y });
+        const mod = (n, m) => (m + (n % m)) % m;
+        const capX = (value, low, high) =>
+          low + mod(value - low, high - low + 1);
+        const capY = (value, low, high) =>
+          low + mod(value - low, high - low + 1);
+
+        obj.css("transform", `rotateX(${capX(x, 0, 360) + "deg"})`);
+      },
+    }
+  );
   $(".calculator-form form > input").on("click", (element) => {
     const val = handleInput(element.target.value);
-    if (!!runningVal[index]) runningVal[index] += "" + val;
-    else runningVal.push(val);
-    const currentVal = runningVal[index];
-    console.log(index);
+    if (
+      !!runningVal[index] &&
+      !["+", "-", "*", "/"].includes(runningVal[index])
+    )
+      runningVal[index] += "" + val;
+    else {
+      runningVal.push(val);
+    }
+    const currentVal = parseFloat(runningVal[index]);
     if (sign == "") $(".total").val(currentVal);
     else {
-      if (sign === "+") runningTotal = add(runningTotal, runningVal[index]);
-      else if (sign === "-")
-        runningTotal = subtract(runningTotal, runningVal[index]);
-      else if (sign === "/")
-        runningTotal = divide(runningTotal, runningVal[index]);
-      else if (sign === "*")
-        runningTotal = multiply(runningTotal, runningVal[index]);
-      $(".total").val($(".total").val() + " " + currentVal);
+      $(".total").val($(".total").val() + val);
     }
-    console.log({ runningTotal, runningVal });
+    if (["+", "-", "*", "/"].includes(runningVal[index])) {
+      index++;
+    }
   });
 
   $(".calculator-form form > button").on("click", (element) => {
-    sign = element.target.value;
-    const currentVal = $(".total").val();
-    if (sign !== "=") {
-      if (index == 0) runningTotal += runningVal[index];
-      index++;
-      $(".total").val(currentVal + " " + sign);
-    } else {
-      $(".total").val(runningTotal);
+    if (runningVal[0]) {
+      sign = element.target.value;
+      const currentVal = $(".total").val();
+      if (sign !== "=") {
+        runningVal.push(sign);
+        if (index == 0) runningTotal += parseFloat(runningVal[index]);
+        index++;
+        $(".total").val(currentVal + " " + sign + " ");
+      } else {
+        let tempTotal = handleCalculate(runningVal);
+        runningVal.push(tempTotal);
+        index = 0;
+        $(".total").val(tempTotal);
+      }
     }
   });
 });
@@ -46,6 +108,22 @@ const handleInput = (input) => {
       total = num;
     }
   }
+  return total;
+};
+
+const handleCalculate = (array) => {
+  let total = 0;
+  if (array.length % 3 == 0 || array.length % 5 == 0)
+    for (let i = 0; i < array.length; ) {
+      let num1 = parseFloat(array[i]);
+      let num2 = parseFloat(array[i + 2]);
+      let sign = array[i + 1];
+      if (sign == "+") total += num1 + num2;
+      else if (sign == "-") total += num1 - num2;
+      else if (sign == "*") total += num1 * num2;
+      else total += num1 / num2;
+      array.splice(0, 3);
+    }
   return total;
 };
 
